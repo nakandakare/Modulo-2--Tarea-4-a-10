2076d2270d558ef339beb591e023bd8f02552e42
nakandakare <kevin.nakandakare@gmail.com>
2021-07-19 21:44:13 -0300

    tarea 10 done

--- utils/generarArchivo.js
+++ utils/generarArchivo.js
@@ -3,7 +3,7 @@ const fs = require('fs').promises;
 
 //Creamos una funcion reutilizable para generar el archivo.
 const generarArchivo = (fileName, dato) => {
-    return fs.writeFile(fileName, dato);
+    return fs.writeFile(`./salida/${fileName}`, dato);
 }
 
 module.exports = generarArchivo;
