258591285863479e4f0676a4a2192062b3795a3f
nakandakare <kevin.nakandakare@gmail.com>
2021-07-20 18:46:14 -0300

    adding airbnb eslint

--- config/yargs.js
+++ config/yargs.js
@@ -1,25 +1,26 @@
-const argv = require('yargs')
-    .option('b', {
-        alias: 'base',
-        type: 'number',
-        demandOption: true,
-        describe: 'Es la base de la tabla de multiplicar'
-    }).option('l', {
-        alias: 'listar',
-        type: 'boolean',
-        default: false,
-        describe: 'Muestra la tabla en consola'
-    })
-    .check((argv, _option) => {
-        if (isNaN(argv.b)) {
-            throw 'La base tiene que ser un numero';
-        }
+const { argv } = require('yargs')
+  .option('b', {
+    alias: 'base',
+    type: 'number',
+    demandOption: true,
+    describe: 'Es la base de la tabla de multiplicar',
+  })
+  .option('l', {
+    alias: 'listar',
+    type: 'boolean',
+    default: false,
+    describe: 'Muestra la tabla en consola',
+  })
+  .check((_argv) => {
+    if (Number.isNaN(_argv.b)) {
+      throw new Error('La base tiene que ser un numero');
+    }
 
-        if (argv.b > 20 || argv.b < 1) {
-            throw 'Debes colocar un valor de base entre 1 y 20';
-        }
+    if (_argv.b > 20 || _argv.b < 1) {
+      throw new Error('Debes colocar un valor de base entre 1 y 20');
+    }
 
-        return true;
-    }).argv
+    return true;
+  });
 
 module.exports = argv;
--- utils/generarArchivo.js
+++ utils/generarArchivo.js
@@ -1,9 +1,7 @@
-//Requerimos FileSystem.
+// Requerimos FileSystem.
 const fs = require('fs').promises;
 
-//Creamos una funcion reutilizable para generar el archivo.
-const generarArchivo = (fileName, dato) => {
-    return fs.writeFile(`./salida/${fileName}`, dato);
-}
+// Creamos una funcion reutilizable para generar el archivo.
+const generarArchivo = (fileName, dato) => fs.writeFile(`./salida/${fileName}`, dato);
 
 module.exports = generarArchivo;
